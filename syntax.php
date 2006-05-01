f1160750b2398217f8b8d6125b964e911170d61a
Michael Klier <chi@chimeric.de>
2006-05-01 21:39:18 +0200

    initial Release
    darcs-hash:20060501193918-484ab-d0856de7976f575f0b6340f44041ca76604f9271.gz

---
+++ syntax.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Syntax Plugin Backlinks
+ * 
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Michael Klier <chi@chimeric.de>
+ */
+
+if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+if(!defined('DW_LF')) define('DW_LF',"\n");
+
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_clearfloat extends DokuWiki_Syntax_Plugin {
+
+
+    /**
+     * General Info
+     */
+    function getInfo(){
+        return array(
+            'author' => 'Michael Klier (chi)',
+            'email'  => 'chi@chimeric.de',
+            'date'   => '2006-05-01',
+            'name'   => 'Clearfloat',
+            'desc'   => 'Clears previous floats form images etc.',
+            'url'    => 'http://www.chimeric.de/dokuwiki/plugins/clearfloat'
+        );
+    }
+
+    /**
+     * Syntax Type
+     *
+     * Needs to return one of the mode types defined in $PARSER_MODES in parser.php
+     */
+    function getType(){ return 'substition'; }
+    function getPType() { return 'block'; }
+    function getSort() { return 308; }
+    
+    /**
+     * Connect pattern to lexer
+     */
+    function connectTo($mode) {
+        $this->Lexer->addSpecialPattern('~~CLEARFLOAT~~',$mode,'plugin_clearfloat');
+    }
+
+    /**
+     * Handler to prepare matched data for the rendering process
+     */
+    function handle($match, $state, $pos, &$handler){
+        return array();
+    }
+
+    /**
+     * Handles the actual output creation.
+     */
+    function render($mode, &$renderer, $data) {
+        global $ID;
+
+        if($mode == 'xhtml'){
+            $renderer->doc .= '<div class="clearer"></div>';
+            return true;
+        }
+        return false;
+    }
+
+}
+//Setup VIM: ex: et ts=4 enc=utf-8 :
